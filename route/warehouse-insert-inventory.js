f81e5fc2de993b4577f64a60608162650f6462d8
kshitijjind <kshitijjind@gmail.com>
2020-06-11 21:15:04 +0530

    warehouse crud

---
+++ route/warehouse-insert-inventory.js
@@ -0,0 +1,85 @@
+var express=require("express");
+var app=express.Router();
+
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+
+//databse connectivity
+mongoose.connect("mongodb://localhost:27017/inventory", { useUnifiedTopology: true,useNewUrlParser: true }).then(()=>{
+    console.log("Connected to database");
+}).catch((err)=>{
+    console.log(err);
+});
+
+//collection schema
+var nameSchema = new mongoose.Schema({
+    itemid:String,
+    name:String,
+    qty:Number,
+    price:String
+});
+
+nameSchema.set('versionKey',false);
+
+var productCol = mongoose.model("products", nameSchema);    //collection
+   
+//insert the product in collection
+app.post("/product_insert",(req,res)=>{
+    console.log(req.body);
+    var myData = new productCol(req.body);
+    myData.save()
+    .then(item => {
+    //res.send({success:true,data:item});
+      res.redirect("/");
+    })
+    .catch(err => {
+    res.status(400).send("unable to save to database");
+    });
+});
+
+//delete the product from collection
+app.post("/product_delete",function(req,res){
+    console.log(req.body);
+    console.log(req.body.itemid);
+
+    productCol.remove({itemid:req.body.itemid}).then(function(result)
+    {
+        res.send(result);
+    }).catch(function()
+    {
+        res.send({err:"error"});
+    });
+    
+});
+
+//update the records in inventory
+app.post("/product_update",function(req,resp){
+    productCol.findOneAndUpdate({itemid: req.body.itemid},{$set:{name:req.body.name,qty:req.body.qty,price:req.body.price}},{new:true}) .then((docs)=>
+    {
+        if(docs) {
+           resp.send({success:true,data:docs});
+        } else
+         {
+            resp.send({success:false,data:"no such user exist"});
+        }
+    })
+         
+});
+
+//search the record in inventory
+app.post("/product_search",function(req,res){
+    productCol.findOne({itemid:req.body.itemid})
+    .then(function(result)
+    {
+        res.send(result);
+    })
+    .catch(function(msg){res.send({err:msg});});
+});
+
+module.exports=app;
+  
---
+++ server.js
@@ -0,0 +1,25 @@
+var express=require("express");
+var path=require("path");
+var bodyparser= require("body-parser");
+
+var app=express();
+
+app.use(express.urlencoded({"extended":false}));
+
+var insert_products=require("./route/warehouse-insert-inventory");
+app.use("/index",insert_products);
+
+app.use(express.static("pages"));       //imp line for .html
+
+app.use("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"pages","index.html"));
+});
+
+var PORT = 8000;
+app.listen(PORT,()=>{
+    console.log("started on " + PORT);
+});
+
+app.use((req,res,next)=>{
+    res.status(404).send("Page not found- invalid URL");
+})
